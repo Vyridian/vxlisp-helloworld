7c94bfc009785bd26c0e719d4c2ab6e8202bc32f
Randall Ratsch <rratsch@gmail.com>
2024-09-20 09:26:28 -0400

    Reworked paths

--- batches/runbuildall.bat
+++ batches/runbuildall.bat
@@ -1,13 +1,13 @@
 cls
 set options=doc srccpp testcpp srccsharp testcsharp srcjava testjava srcjs testjs srckotlin testkotlin
-set runablefolder=..\..\vxlisp-runtime
-rem set runablefolder=..\..\vxlisp-compiler
+set runablefolder=../../vxlisp/dist
+rem set runablefolder=../../vxlisp/src
 set currentfolder=%cd%
-set srcfolder=%currentfolder%\..\src
+set srcfolder=%currentfolder%/../src
 set runable=vxlisp_win64.exe
 rem set runable=go run .
 set pipe=
-rem set pipe=> %currentfolder%\run.log
+rem set pipe=> %currentfolder%/run.log
 cd %runablefolder%
 %runable% %options% :path %srcfolder% %pipe%
 cd %currentfolder%
